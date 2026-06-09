b612a8b499c6a7c266efe57bf5e67376dea08b86
ArcaneCrowA <arcanecrowa@icloud.com>
2026-06-09 18:20:34 +0500

    codecrafters submit [skip ci]

--- internal/commands/type.go
+++ internal/commands/type.go
@@ -1,8 +1,13 @@
 package commands
 
 import (
+	"errors"
 	"fmt"
+	"log/slog"
+	"os"
+	"path"
 	"slices"
+	"strings"
 )
 
 var builtin = []string{"exit", "echo", "type"}
@@ -10,7 +15,31 @@ var builtin = []string{"exit", "echo", "type"}
 func Type(command string) {
 	if slices.Contains(builtin, command) {
 		fmt.Printf("%s is a shell builtin\n", command)
-	} else {
-		Invalid(command)
+		return
 	}
+	if p, e := find(command); e {
+		fmt.Printf("%s is %s\n", command, p)
+		return
+	}
+
+	Invalid(command)
+}
+
+func find(command string) (string, bool) {
+	paths := strings.SplitSeq(os.Getenv("PATH"), string(os.PathSeparator))
+	for p := range paths {
+		fullpath := path.Join(p, command)
+		file, err := os.Stat(fullpath)
+		if err != nil {
+			if errors.Is(err, os.ErrNotExist) {
+				continue
+			}
+			slog.Error(err.Error())
+			os.Exit(1)
+		}
+		if !file.IsDir() && file.Mode()&0001 != 0 {
+			return p, true
+		}
+	}
+	return "", false
 }
