df225079a034e411843eeda1dcaf45fa7137eec1
ArcaneCrowA <arcanecrowa@icloud.com>
2026-06-09 15:53:10 +0500

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 func main() {
-	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
+
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	command = strings.TrimSpace(command)
+
+	fmt.Printf("%s: command not found", command)
 }
