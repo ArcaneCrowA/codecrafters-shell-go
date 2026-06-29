edc5353b953228d24d77d30dbc9021f9af60a95c
ArcaneCrowA <arcanecrowa@icloud.com>
2026-06-29 12:42:06 +0500

    codecrafters submit [skip ci]

--- internal/commands/shared.go
+++ internal/commands/shared.go
@@ -39,7 +39,12 @@ func writeOutput(line string, redirect int, file string) {
 	case 0:
 		fmt.Println(line)
 	case 1:
-		fullpath := path.Join(pwd(), file)
+		var fullpath string
+		if path.IsAbs(file) {
+			fullpath = file
+		} else {
+			fullpath = path.Join(pwd(), file)
+		}
 		if err := os.WriteFile(fullpath, []byte(line), 0644); err != nil {
 			slog.Error("failed to write to file", "err", err.Error())
 			os.Exit(1)
