99f91ec47e5db8352d6102c1160377ff8ac05c43
ArcaneCrowA <arcanecrowa@icloud.com>
2026-07-16 22:21:55 +0500

    codecrafters submit [skip ci]

--- internal/commands/exec.go
+++ internal/commands/exec.go
@@ -13,16 +13,25 @@ func Exec(args []string, redirect int, file string) {
 	}
 
 	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Stderr = os.Stderr
-	if redirect > 0 {
+	switch redirect {
+	case 0:
+		cmd.Stdout = os.Stdout
+		_ = cmd.Run()
+	case 1:
+		cmd.Stderr = os.Stderr
 		output, err := cmd.Output()
 		if len(output) > 0 {
 			writeOutput(string(output), redirect, file)
 		} else if err != nil {
 			writeOutput("", redirect, file)
 		}
-	} else {
-		cmd.Stdout = os.Stdout
-		_ = cmd.Run()
+	case 2:
+		cmd.Stdout = os.Stderr
+		output, err := cmd.Output()
+		if len(output) > 0 {
+			writeOutput(string(output), redirect, file)
+		} else if err != nil {
+			writeOutput("", redirect, file)
+		}
 	}
 }
